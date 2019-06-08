d7edfe431310512111873bdf3503a5ea99e70d2a
Your Name <tfikrie@yahoo.com>
2019-06-07 20:40:50 -0400

    css styling is done for starwars data

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -3,17 +3,19 @@ import './StarWars.css';
 
 function StarWars(props) {
     return (
-        <div>
-        <h3>{props.starWar.name}</h3>
-        <p>
-          <strong>Height:</strong> {props.starWar.height}
-        </p>
-        <p>
-          <strong>Gender:</strong> {props.starWar.gender}
-        </p>
-        <p>
-          <strong>Mass:</strong> {props.starWar.mass}
-        </p>
+        <div className={'StarWars'}>
+         <h3 className={'name'} >{props.starWar.name}</h3>
+         <div className={'starNamedata'}>
+          <p>
+            <strong>Height:</strong> {props.starWar.height}
+          </p>
+          <p>
+            <strong>Gender:</strong> {props.starWar.gender}
+          </p>
+          <p>
+            <strong>Mass:</strong> {props.starWar.mass}
+          </p>
+         </div>
         </div>
     )
 }
